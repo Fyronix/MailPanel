9be0ecd35d0f3094e86d82aaedc56f7c26a585d7
asabagh <a.sabagh72@gmail.com>
2017-07-07 01:53:07 +0430

    initialize MailPanel

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+tinymce.init({
+    selector: 'textarea.tinymce',
+    min_height: 350,
+    plugins: "table , link , lists , hr , paste , print , textcolor , code , emoticons , searchreplace , spellchecker",
+    toolbar: "undo redo | styleselect | searchreplace spellchecker | bold italic | emoticons hr | link unlink | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | fontsizeselect"
+});
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MailPanel</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css"/>
+    </head>
+    <body>
+        <textarea class="tinymce"></textarea>
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/chosen.jquery.min.js"></script>
+        <script src="assets/js/tinymce.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+    </body>
+</html>
+
