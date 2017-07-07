59e0a024014a81984f7fc9a746a99369515553a4
asabagh <a.sabagh72@gmail.com>
2017-07-07 19:35:56 +0430

    additional requirement

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,4 +4,6 @@ tinymce.init({
     plugins: "table , link , lists , hr , paste , print , textcolor , code , emoticons , searchreplace , spellchecker",
     toolbar: "undo redo | styleselect | searchreplace spellchecker | bold italic | emoticons hr | link unlink | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | fontsizeselect"
 });
-
+$(document).ready(function(){
+    $(".chosen-select").chosen({max_selected_options: 5});
+});
